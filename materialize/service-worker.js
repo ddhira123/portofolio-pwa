974713b75fa7c9d41abb455eb8d359c7cffd3e77
ddhira123 <dhana_dhira@apps.ipb.ac.id>
2020-10-22 16:58:33 +0700

    First commit

---
+++ materialize/service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'firstpwa';
+var urlsToCache = [
+	'/',
+	'/nav.html',
+	'/index.html',
+	'/pages/home.html',
+	'/pages/about.html',
+	'/pages/projects.html',
+	'/pages/certifications.html',
+	'/pages/experiences.html',
+	'/js/materialize.min.js',
+	'/js/jquery-3.5.1.min.js',
+	'/js/script.js',
+	'/css/materialize.min.css',
+	'/css/styles.css'
+];
+
+self.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
+			return cache.addAll(urlsToCache);
+		})
+	);
+})
+
+self.addEventListener('activate', function(event){
+	event.waitUntil(
+		caches.keys()
+		.then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.map(function(cacheName){
+					if(cacheName != CACHE_NAME){	
+						console.log("ServiceWorker: cache " + cacheName + " dihapus");
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
+})
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request, {cacheName:CACHE_NAME})
+		.then(function(response) {
+			if(response){
+				console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+				return response;
+			}
+			
+			console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
+			return fetch(event.request);
+		})
+	);
+});
